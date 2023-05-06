2d2cd9e4bb6b074c71b38246b9053a3f2b758396
Jack Tomsett <jacktomsett93@gmail.com>
2023-05-06 20:53:34 +0100

    Refactored code to put making api request into a function. Next want to make functions out of all api calls.

--- libcurl-testing/main.cpp
+++ libcurl-testing/main.cpp
@@ -7,46 +7,44 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
     return size * nmemb;
 }
 
-
-std::string BuildQueryString(std::string targetbody)
+std::string astronomyapirequest(std::string URL, std::string HEADER)
 {
-    return "https://api.astronomyapi.com/api/v2/bodies/positions/" + targetbody + "?longitude=-84.39733&latitude=33.775867&elevation=1&from_date=2023-05-04&to_date=2023-05-04&time=13%3A57%3A57";
-}
-
-int main()
-{
-    //Define Targets
-    std::string Target = "jupiter";
-
-    std::string URLstring = BuildQueryString(Target);
-    std::cout << URLstring << std::endl;
-    URLstring = "https://api.astronomyapi.com/api/v2/bodies/positions/jupiter?longitude=-84.39733&latitude=33.775867&elevation=1&from_date=2023-05-04&to_date=2023-05-04&time=13%3A57%3A57";
-
-
-
-
-
-
     // Fetch data
     CURL * curl;
-    struct curl_slist *headerlist = NULL;
-    
+    struct curl_slist *headerlist = NULL;    
     CURLcode res;
     std::string readBuffer;
-
-    headerlist = curl_slist_append(headerlist, "Authorization: Basic YzJhNTM2NzktNTQzYS00MDBmLThmMDMtNjI1ZTc2MmFhYjkxOjE5NWQ4ZjNhYjFmM2M1Y2YyNjdiODEwY2IzNDM2OGExMWExNmUwNDNlY2Q1ZWRiOGJmNDVlMzZiOGMxOTliMGFhMjU0NzFhNTZmOTc1YzBlNzFjNTYwZmJlYmY1ZjdkNjJhM2JkZTQzMTY3NDhhMjhiMWNlNTljYTExNzUwYWRkOWExZWI3YzVhOGU3NDM3ZGVmOGM1YWQ1ZTVlYzQ5ZGQ0ZWNjZDg3YTg4OTIzOGQ1YjA0ZWI0ZGFhMDIxMGQzNjdhZjMwZmVhYTZhNmM1YjI3YzdiODkwYjllMzU0MzJl");
+    headerlist = curl_slist_append(headerlist, HEADER.c_str());
     curl = curl_easy_init();
     if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, URLstring.c_str());
+        curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
-
-        std::cout << readBuffer << std::endl;
     }
+    return readBuffer;
+}
+
+
+std::string getPosition(std::string Target, std::string AuthString)
+{
+    std::string URLstring = "https://api.astronomyapi.com/api/v2/bodies/positions/" + Target + "?longitude=-84.39733&latitude=33.775867&elevation=1&from_date=2023-05-04&to_date=2023-05-04&time=13%3A57%3A57";
+    return astronomyapirequest(URLstring,AuthString);
+}
+
+
+
+int main()
+{
+    //Define Targets
+    std::string target = "jupiter";
+    std::string authorisation_string = "Authorization: Basic YzJhNTM2NzktNTQzYS00MDBmLThmMDMtNjI1ZTc2MmFhYjkxOjE5NWQ4ZjNhYjFmM2M1Y2YyNjdiODEwY2IzNDM2OGExMWExNmUwNDNlY2Q1ZWRiOGJmNDVlMzZiOGMxOTliMGFhMjU0NzFhNTZmOTc1YzBlNzFjNTYwZmJlYmY1ZjdkNjJhM2JkZTQzMTY3NDhhMjhiMWNlNTljYTExNzUwYWRkOWExZWI3YzVhOGU3NDM3ZGVmOGM1YWQ1ZTVlYzQ5ZGQ0ZWNjZDg3YTg4OTIzOGQ1YjA0ZWI0ZGFhMDIxMGQzNjdhZjMwZmVhYTZhNmM1YjI3YzdiODkwYjllMzU0MzJl";
+    std::string position_string = getPosition(target,authorisation_string);
+
+    std::cout << position_string << std::endl;
+
 
     return 0;
-
 }
