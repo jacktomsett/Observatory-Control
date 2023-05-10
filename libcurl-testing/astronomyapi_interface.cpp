c579bc4063f9565bcdca4c80ca32503215863e61
Jack Tomsett <jacktomsett93@gmail.com>
2023-05-10 08:10:11 +0100

    Recreated all functionallity of previous file in astronomyapi_interface.cpp

--- libcurl-testing/astronomyapi_interface.cpp
+++ libcurl-testing/astronomyapi_interface.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <curl/curl.h>
+#include <Date.h>
+#include <Date.cpp>
 
 //Define observer paramater structure
 struct ObserverParams
@@ -12,14 +14,22 @@ struct ObserverParams
     std::string time;
 };
 
+std::string buildQueryString(ObserverParams observer)
+{
+    //convert :'s in time string
+    std::string timestring = observer.time;
+    timestring.replace(3,1,"%3A");
+    timestring.replace(7,1,"%3A");
+    return "?longitude=" + std::to_string(observer.Longitude) + "&lattitude=" + std::to_string(observer.Lattitude) + "&elevation=" + std::to_string(observer.Elevation) + "&from_date=" + observer.StartDate.print() + "&to_date=" + observer.EndDate.print() + "&time=" + timestring;
+
+}
 
 //=========To Do.....
 std::string generateAuthString(std::string ApplicationID, std::string ApplicationSecret)
 {
 
-    return "0"
+    return "Authorization: Basic YzJhNTM2NzktNTQzYS00MDBmLThmMDMtNjI1ZTc2MmFhYjkxOjE5NWQ4ZjNhYjFmM2M1Y2YyNjdiODEwY2IzNDM2OGExMWExNmUwNDNlY2Q1ZWRiOGJmNDVlMzZiOGMxOTliMGFhMjU0NzFhNTZmOTc1YzBlNzFjNTYwZmJlYmY1ZjdkNjJhM2JkZTQzMTY3NDhhMjhiMWNlNTljYTExNzUwYWRkOWExZWI3YzVhOGU3NDM3ZGVmOGM1YWQ1ZTVlYzQ5ZGQ0ZWNjZDg3YTg4OTIzOGQ1YjA0ZWI0ZGFhMDIxMGQzNjdhZjMwZmVhYTZhNmM1YjI3YzdiODkwYjllMzU0MzJl";
 }
-
 
 //Functions to make http requests
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp){
@@ -47,4 +57,21 @@ std::string astronomyapirequest(std::string URL, std::string AuthString)
     return readBuffer;
 }
 
-//AstronomyAPI functions
+//====================AstronomyAPI functions=======================//
+//Bodies
+
+std::string getBodyPosition(std::string body, ObserverParams Observer, std::string AuthString)
+{
+    std::string URL = "https://api.astronomyapi.com/api/v2/bodies/positions/" + body + buildQueryString(Observer);
+    return astronomyapirequest(URL,AuthString);
+}
+
+
+//Studio
+
+
+//Search
+
+
+
+
