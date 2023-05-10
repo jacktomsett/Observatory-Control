8261b4dfed8b0ae81907d95bf243d54bb95d2c26
Jack Tomsett <jacktomsett93@gmail.com>
2023-05-10 08:44:33 +0100

    Created new sample main program in main.cpp using the new clean code defined in astronomyapi_interface.cpp

--- libcurl-testing/main.cpp
+++ libcurl-testing/main.cpp
@@ -1,45 +1,26 @@
 #include <iostream>
 #include <string>
 #include <curl/curl.h>
+#include <astronomyapi_interface.cpp>
 
-
-
-
-
-std::string astronomyapirequest(std::string URL)
-{
-    std::string HEADER = "Authorization: Basic YzJhNTM2NzktNTQzYS00MDBmLThmMDMtNjI1ZTc2MmFhYjkxOjE5NWQ4ZjNhYjFmM2M1Y2YyNjdiODEwY2IzNDM2OGExMWExNmUwNDNlY2Q1ZWRiOGJmNDVlMzZiOGMxOTliMGFhMjU0NzFhNTZmOTc1YzBlNzFjNTYwZmJlYmY1ZjdkNjJhM2JkZTQzMTY3NDhhMjhiMWNlNTljYTExNzUwYWRkOWExZWI3YzVhOGU3NDM3ZGVmOGM1YWQ1ZTVlYzQ5ZGQ0ZWNjZDg3YTg4OTIzOGQ1YjA0ZWI0ZGFhMDIxMGQzNjdhZjMwZmVhYTZhNmM1YjI3YzdiODkwYjllMzU0MzJl";
-    // Fetch data
-    CURL * curl;
-    struct curl_slist *headerlist = NULL;    
-    CURLcode res;
-    std::string readBuffer;
-    headerlist = curl_slist_append(headerlist, HEADER.c_str());
-    curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-    }
-    return readBuffer;
-}
-
-
-std::string getPosition(std::string Target, std::string Location)
-{
-    std::string URLstring = "https://api.astronomyapi.com/api/v2/bodies/positions/" + Target + "?longitude=-84.39733&latitude=33.775867&elevation=1&from_date=2023-05-04&to_date=2023-05-04&time=13%3A57%3A57";
-    return astronomyapirequest(URLstring);
-}
 
 int main()
 {
-    //Define Targets
+    // api credentials...
+    std::string AppID = "c2a53679-543a-400f-8f03-625e762aab91";
+    std::string AppSecretKey = "195d8f3ab1f3c5cf267b810cb34368a11a16e043ecd5edb8bf45e36b8c199b0aa25471a56f975c0e71c560fbebf5f7d62a3bde4316748a28b1ce59ca11750add9a1eb7c5a8e7437def8c5ad5e5ec49dd4eccd87a889238d5b04eb4daa0210d367af30feaa6a6c5b27c7b890b9e35432e";
+
+    // obvserver data...
+    ObserverParams Whiston;
+    Whiston.Lattitude = 53.412791;
+    Whiston.Longitude = 2.801705;
+    Whiston.Elevation = 1;
+    Whiston.StartDate = Date(2023,5,10);
+    Whiston.EndDate = Date(2023,05,11);
+    Whiston.time = "21:30:00";
+
     std::string target = "jupiter";
-    std::string location = "temp";
-    std::string position_string = getPosition(target,location);
+    std::string position_string = getBodyPosition(target,Whiston,generateAuthString(AppID,AppSecretKey));
 
     std::cout << position_string << std::endl;
 
