9e201fa2074c50a495f389feb510cc322eecf827
Jack Tomsett <jacktomsett93@gmail.com>
2024-08-22 17:39:33 +0100

    Started adding an action server to camera node. Following along with the tutorial but adjusting it to take a sequence of images. Only commiting it half finished so I can carry on later from a different machine. Added the callback functions, and I'm now working on the execute function.

--- ros2observatory_ws/src/data_camera/src/camera.cpp
+++ ros2observatory_ws/src/data_camera/src/camera.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <functional>
 #include <memory>
+#include <thread>
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
@@ -25,10 +26,18 @@ class DataCamera : public rclcpp::Node
       batteryservice      = this->create_service<interfaces::srv::BatteryRequest>(
         "battery_status", std::bind(&DataCamera::battery_callback, this, std::placeholders::_1, std::placeholders::_2)
       );
+      sequenceaction      = rclcpp_action::create_server<interfaces::action::Sequence>(
+        this, "sequence",
+        std::bind(&DataCamera::sequence_goal, this, std::placeholders::_1, std::placeholders::_2),
+        std::bind(&DataCamera::sequence_cancel, this, std::placeholders::_1, std::placeholders::_2),
+        std::bind(&DataCamera::sequence_accepted, this, std::placeholders::_1, std::placeholders::_2)
+      )
 
     }
 
   private:
+    //  HELPER FUNCTIONS
+    
     void capture_image()
     {
       auto message = interfaces::msg::Placeholder();
@@ -37,7 +46,13 @@ class DataCamera : public rclcpp::Node
       imagepublisher->publish(message);
     }
 
-    //   SERVICE CALLBACKS
+    //  PUBLISHERS, SUBSCRIBERS SERVICES AND ACTIONS
+    rclcpp::Publisher<interfaces::msg::Placeholder>::SharedPtr imagepublisher;
+    rclcpp::Service<interfaces::srv::Confirmation>::SharedPtr startcaptureservice;
+    rclcpp::Service<interfaces::srv::BatteryRequest>::SharedPtr batteryservice;
+    rclcpp_action::Server<interfaces::action::Sequence>::SharedPtr sequenceaction;
+
+    //  SERVICE CALLBACKS
     void startcapture_callback(const std::shared_ptr<interfaces::srv::Confirmation::Request> request,
       std::shared_ptr<interfaces::srv::Confirmation::Response> response)
     {
@@ -58,12 +73,44 @@ class DataCamera : public rclcpp::Node
       RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Responding with: %ld", (long int)response->percentage);
     }
 
+    //  ACTION CALLBACKS
+    void sequence_accepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<interfaces::action::Sequence>> goal_handle)
+    {
+      //This callback needs to finish quickly so it does not freeze up the system.
+      //Interactions with gphoto will be done in a different thread.
+      std::thread{std::bind(&DataCamera::capture_sequence, this, std::placeholders::-1),goal_handle}.detach();
+    }
 
-    rclcpp::Publisher<interfaces::msg::Placeholder>::SharedPtr imagepublisher;
-    rclcpp::Service<interfaces::srv::Confirmation>::SharedPtr startcaptureservice;
-    rclcpp::Service<interfaces::srv::BatteryRequest>::SharedPtr batteryservice;
-    
+    rclcpp_action::GoalResponse sequence_goal(
+      const rclcpp_action::GoalUUID & uuid,
+      std::shared_ptr<const interfaces::action::Sequence::Goal> goal
+    )
+    {
+      RCLCPP_INFO(this->get_logger(), "Received request for sequence of %d", goal->length);
+      (void)uuid;
+      return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+    }//This has been copied from tutorial, and just accepts all goals. Obviously the camera cannot take two sequences
+    // simulataneously. We need to either cancel the current goal and start a new once if one exists, or just refuse a
+    // a new goal if a current goal exists
 
+    rclcpp_action::CancelResponse sequence_cancel(
+      const std::shared_ptr<rclcpp_action::ServerGoalHandle<interfaces::action::Sequence>> goal_handle
+    )
+    {
+      RCLCPP_INFO(this->get_logger(), "Received request to cancel sequence");
+      (void)goal_handle;
+      return rclcpp_action::CancelResponse::ACCEPT;
+    }//Accoring to tutorial, this just tells the client that the cancellation has been accepted. Presumable to actually
+    //cancel something you would need to do something with the goal_handle parameter.
+
+    void capture_sequence(const std::shared_ptr<rclcpp_action::ServerGoalHandle<interfaces::action::Sequence>> goal_handle)
+    {
+      RCLCPP_INFO(this->get_logger(), "Executing sequence");
+      rclcpp:Rate loop_rate(1);
+      const auto goal = goal_handle->get_goal();
+      auto feedback = std::make_shared<interfaces::action::Sequence::Feedback>();
+      
+    }
 };
 
 int main(int argc, char * argv[])
