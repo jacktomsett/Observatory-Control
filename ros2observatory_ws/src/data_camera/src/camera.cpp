ae259a56948e663fe1b89de201fab69ee34d1b79
Jack Tomsett <jacktomsett93@gmail.com>
2024-08-22 15:58:41 +0100

    Added a placeholder battery reporting service to the camera node in data_camera. For some reason I can only get it to work if the callback is defined outside of the class.

--- ros2observatory_ws/src/data_camera/src/camera.cpp
+++ ros2observatory_ws/src/data_camera/src/camera.cpp
@@ -11,6 +11,15 @@ using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
+    void battery_callback(const std::shared_ptr<interfaces::srv::BatteryRequest::Request> request,
+      std::shared_ptr<interfaces::srv::BatteryRequest::Response> response)
+    {
+      int battery = 69;
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Battery status requested");
+      response->percentage = battery;
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Responding with: %ld", (long int)response->percentage);
+    }
+
 
 class DataCamera : public rclcpp::Node
 {
@@ -18,9 +27,11 @@ class DataCamera : public rclcpp::Node
     DataCamera()
     : Node("data_camera")
     {
-      publisher_ = this->create_publisher<interfaces::msg::Placeholder>("data_stream", 10);
+      datapublisher   = this->create_publisher<interfaces::msg::Placeholder>("data_stream", 10);
+      batteryservice  = this->create_service<interfaces::srv::BatteryRequest>(
+        "battery_status", &battery_callback);
       timer_ = this->create_wall_timer(
-      2000ms, std::bind(&DataCamera::timer_callback, this));
+        2000ms, std::bind(&DataCamera::timer_callback, this));
     }
 
   private:
@@ -29,10 +40,12 @@ class DataCamera : public rclcpp::Node
       auto message = interfaces::msg::Placeholder();
       message.image = "Published image";
       RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: " << message.image);
-      publisher_->publish(message);
+      datapublisher->publish(message);
     }
+
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<interfaces::msg::Placeholder>::SharedPtr publisher_;
+    rclcpp::Publisher<interfaces::msg::Placeholder>::SharedPtr datapublisher;
+    rclcpp::Service<interfaces::srv::BatteryRequest>::SharedPtr batteryservice;
     size_t count_;
 };
 
