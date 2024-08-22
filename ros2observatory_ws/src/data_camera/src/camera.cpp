180aab8b3cdb7ca17258e4b9e23bd7adfbb72d6d
Jack Tomsett <jacktomsett93@gmail.com>
2024-08-22 12:43:40 +0100

    Fixxed error with placeholder message. It turns out .msg files must start with a capital letter

---
+++ ros2observatory_ws/src/data_camera/src/camera.cpp
@@ -0,0 +1,44 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "interfaces/srv/BatteryRequest.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+class DataCamera : public rclcpp::Node
+{
+  public:
+    DataCamera()
+    : Node("data_camera")
+    {
+      publisher_ = this->create_publisher<interfaces::msg::placeholder>("data_stream", 10);
+      timer_ = this->create_wall_timer(
+      2000ms, std::bind(&DataCamera::timer_callback, this));
+    }
+
+  private:
+    void timer_callback()
+    {
+      auto message = interfaces::msg::Placeholder();
+      message.image = "Published image";
+      RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: " << message.image);
+      publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<DataCamera>());
+  rclcpp::shutdown();
+  return 0;
+}
