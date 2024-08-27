01c37d8962705998c3feb3fbd4f0376575e2f888
Jack Tomsett <jacktomsett93@gmail.com>
2024-08-27 19:07:09 +0100

    Added basic entry to gphoto interface to camewra node. Spent a lot of time trying to get it play nice with cmake. Can't test it on my pc so commiting it now to move it to my laptop to test.

--- ros2observatory_ws/src/data_camera/src/camera.cpp
+++ ros2observatory_ws/src/data_camera/src/camera.cpp
@@ -3,8 +3,13 @@
 #include <memory>
 #include <thread>
 #include <string>
-#include <iostream>
+#include <stdlib.h>
 
+#include <gphoto2/gphoto2-camera.h>
+//include "samples.h"
+
+
+//======= ROS2 includes===========//
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
@@ -18,7 +23,40 @@
 
 using namespace std::chrono_literals;
 
+static void
+ctx_error_func (GPContext *context, const char *str, void *data)
+{
+        fprintf  (stderr, "\n*** Contexterror ***              \n%s\n",str);
+        fflush   (stderr);
+}
 
+static void
+ctx_status_func (GPContext *context, const char *str, void *data)
+{
+        fprintf  (stderr, "%s\n", str);
+        fflush   (stderr);
+}
+
+GPContext* sample_create_context() {
+	GPContext *context;
+
+	/* This is the mandatory part */
+	context = gp_context_new();
+
+	/* All the parts below are optional! */
+        gp_context_set_error_func (context, ctx_error_func, NULL);
+        gp_context_set_status_func (context, ctx_status_func, NULL);
+
+	/* also:
+	gp_context_set_cancel_func    (p->context, ctx_cancel_func,  p);
+        gp_context_set_message_func   (p->context, ctx_message_func, p);
+        if (isatty (STDOUT_FILENO))
+                gp_context_set_progress_funcs (p->context,
+                        ctx_progress_start_func, ctx_progress_update_func,
+                        ctx_progress_stop_func, p);
+	 */
+	return context;
+}
 
 class DataCamera : public rclcpp::Node
 {
@@ -26,6 +64,7 @@ class DataCamera : public rclcpp::Node
     DataCamera()
     : Node("data_camera")
     {
+      //=============== ROS Stuff==============//
       //Initialise publishers
       imagepublisher      = this->create_publisher<interfaces::msg::Placeholder>("data_stream", 10);
       eventpublisher      = this->create_publisher<interfaces::msg::Event>("camera_events", 10);
@@ -53,9 +92,24 @@ class DataCamera : public rclcpp::Node
         std::bind(&DataCamera::sequence_accepted, this, std::placeholders::_1)
       );
 
+      //============ gphoto2 stuff===============//
+      //Detect camera
+      context = sample_create_context();
+
+
     }
 
   private:
+    // gphoto2 object handles
+    Camera      *camera;
+    int         ret;
+    GPContext   *context;
+    CameraText  text;
+    
+    
+    
+    
+    
     //  HELPER FUNCTIONS
     
     void capture_image()
