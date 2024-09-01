fc4595fa50d04f8171343bb311930c1eb0397bda
Jack Tomsett <jacktomsett93@gmail.com>
2024-09-01 18:31:08 +0100

    Pulled out the libgphoto helper functions to their own file. Added a couple more from github samples and added the src directory as an include in github so that it will build

--- ros2observatory_ws/src/data_camera/src/camera.cpp
+++ ros2observatory_ws/src/data_camera/src/camera.cpp
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 #include <gphoto2/gphoto2-camera.h>
-//include "samples.h"
+#include <libgphoto2_functions.cpp>
 
 
 //======= ROS2 includes===========//
@@ -24,42 +24,7 @@
 using namespace std::chrono_literals;
 
 
-//Following functions copied from libgphoto samples from github. They seem to be gphotos error log system. Eventually
-//need to figure out how to get it to write to ros info
-static void
-ctx_error_func (GPContext *context, const char *str, void *data)
-{
-        fprintf  (stderr, "\n*** Contexterror ***              \n%s\n",str);
-        fflush   (stderr);
-}
 
-static void
-ctx_status_func (GPContext *context, const char *str, void *data)
-{
-        fprintf  (stderr, "%s\n", str);
-        fflush   (stderr);
-}
-
-GPContext* sample_create_context() {
-	GPContext *context;
-
-	/* This is the mandatory part */
-	context = gp_context_new();
-
-	/* All the parts below are optional! */
-        gp_context_set_error_func (context, ctx_error_func, NULL);
-        gp_context_set_status_func (context, ctx_status_func, NULL);
-
-	/* also:
-	gp_context_set_cancel_func    (p->context, ctx_cancel_func,  p);
-        gp_context_set_message_func   (p->context, ctx_message_func, p);
-        if (isatty (STDOUT_FILENO))
-                gp_context_set_progress_funcs (p->context,
-                        ctx_progress_start_func, ctx_progress_update_func,
-                        ctx_progress_stop_func, p);
-	 */
-	return context;
-}
 // End of functions copied from libgphoto examples
 
 class DataCamera : public rclcpp::Node
@@ -147,6 +112,7 @@ class DataCamera : public rclcpp::Node
         eventpublisher->publish(eventmessage);
 
         //Get root widget...
+        /*
         ret = gp_camera_get_config(camera, &configwindow, context);
         //Widget should now contain info
         const char * widgetname;
@@ -162,6 +128,7 @@ class DataCamera : public rclcpp::Node
           std::string childname(childwidgetname);
           std::cout << "  " << childname << std::endl;
         }
+        */
 
         return true;
       }
@@ -198,7 +165,7 @@ class DataCamera : public rclcpp::Node
       RCLCPP_INFO(this->get_logger(), "Battery status requested");
       if(isCameraConnected){
         //Here is where we would get the batery info from the camera
-        char* label = 
+        
 
         //Create widget for battery level setting
 
---
+++ ros2observatory_ws/src/data_camera/src/libgphoto2_functions.cpp
@@ -0,0 +1,177 @@
+//Following functions copied from libgphoto samples from github. They seem to be gphotos error log system. Eventually
+//need to figure out how to get it to write to ros info
+static void
+ctx_error_func (GPContext *context, const char *str, void *data)
+{
+        fprintf  (stderr, "\n*** Contexterror ***              \n%s\n",str);
+        fflush   (stderr);
+}
+
+static void
+ctx_status_func (GPContext *context, const char *str, void *data)
+{
+        fprintf  (stderr, "%s\n", str);
+        fflush   (stderr);
+}
+
+GPContext* sample_create_context() {
+	GPContext *context;
+
+	/* This is the mandatory part */
+	context = gp_context_new();
+
+	/* All the parts below are optional! */
+        gp_context_set_error_func (context, ctx_error_func, NULL);
+        gp_context_set_status_func (context, ctx_status_func, NULL);
+
+	/* also:
+	gp_context_set_cancel_func    (p->context, ctx_cancel_func,  p);
+        gp_context_set_message_func   (p->context, ctx_message_func, p);
+        if (isatty (STDOUT_FILENO))
+                gp_context_set_progress_funcs (p->context,
+                        ctx_progress_start_func, ctx_progress_update_func,
+                        ctx_progress_stop_func, p);
+	 */
+	return context;
+}
+
+static int
+_lookup_widget(CameraWidget*widget, const char *key, CameraWidget **child) {
+	int ret;
+	ret = gp_widget_get_child_by_name (widget, key, child);
+	if (ret < GP_OK)
+		ret = gp_widget_get_child_by_label (widget, key, child);
+	return ret;
+}
+
+/* Gets a string configuration value.
+ * This can be:
+ *  - A Text widget
+ *  - The current selection of a Radio Button choice
+ *  - The current selection of a Menu choice
+ *
+ * Sample (for Canons eg):
+ *   get_config_value_string (camera, "owner", &ownerstr, context);
+ */
+int
+get_config_value_string (Camera *camera, const char *key, char **str, GPContext *context) {
+	CameraWidget		*widget = NULL, *child = NULL;
+	CameraWidgetType	type;
+	int			ret;
+	char			*val;
+
+	ret = gp_camera_get_single_config (camera, key, &child, context);
+	if (ret == GP_OK) {
+		if (!child) fprintf(stderr,"child is NULL?\n");
+		widget = child;
+	} else {
+		ret = gp_camera_get_config (camera, &widget, context);
+		if (ret < GP_OK) {
+			fprintf (stderr, "camera_get_config failed: %d\n", ret);
+			return ret;
+		}
+		ret = _lookup_widget (widget, key, &child);
+		if (ret < GP_OK) {
+			fprintf (stderr, "lookup widget failed: %d\n", ret);
+			goto out;
+		}
+	}
+
+	/* This type check is optional, if you know what type the label
+	 * has already. If you are not sure, better check. */
+	ret = gp_widget_get_type (child, &type);
+	if (ret < GP_OK) {
+		fprintf (stderr, "widget get type failed: %d\n", ret);
+		goto out;
+	}
+	switch (type) {
+        case GP_WIDGET_MENU:
+        case GP_WIDGET_RADIO:
+        case GP_WIDGET_TEXT:
+		break;
+	default:
+		fprintf (stderr, "widget has bad type %d\n", type);
+		ret = GP_ERROR_BAD_PARAMETERS;
+		goto out;
+	}
+
+	/* This is the actual query call. Note that we just
+	 * a pointer reference to the string, not a copy... */
+	ret = gp_widget_get_value (child, &val);
+	if (ret < GP_OK) {
+		fprintf (stderr, "could not query widget value: %d\n", ret);
+		goto out;
+	}
+	/* Create a new copy for our caller. */
+	*str = strdup (val);
+out:
+	gp_widget_free (widget);
+	return ret;
+}
+
+
+/* Sets a string configuration value.
+ * This can set for:
+ *  - A Text widget
+ *  - The current selection of a Radio Button choice
+ *  - The current selection of a Menu choice
+ *
+ * Sample (for Canons eg):
+ *   get_config_value_string (camera, "owner", &ownerstr, context);
+ */
+int
+set_config_value_string (Camera *camera, const char *key, const char *val, GPContext *context) {
+	CameraWidget		*widget = NULL, *child = NULL;
+	CameraWidgetType	type;
+	int			ret;
+
+	ret = gp_camera_get_config (camera, &widget, context);
+	if (ret < GP_OK) {
+		fprintf (stderr, "camera_get_config failed: %d\n", ret);
+		return ret;
+	}
+	ret = _lookup_widget (widget, key, &child);
+	if (ret < GP_OK) {
+		fprintf (stderr, "lookup widget failed: %d\n", ret);
+		goto out;
+	}
+
+	/* This type check is optional, if you know what type the label
+	 * has already. If you are not sure, better check. */
+	ret = gp_widget_get_type (child, &type);
+	if (ret < GP_OK) {
+		fprintf (stderr, "widget get type failed: %d\n", ret);
+		goto out;
+	}
+	switch (type) {
+        case GP_WIDGET_MENU:
+        case GP_WIDGET_RADIO:
+        case GP_WIDGET_TEXT:
+		break;
+	default:
+		fprintf (stderr, "widget has bad type %d\n", type);
+		ret = GP_ERROR_BAD_PARAMETERS;
+		goto out;
+	}
+
+	/* This is the actual set call. Note that we keep
+	 * ownership of the string and have to free it if necessary.
+	 */
+	ret = gp_widget_set_value (child, val);
+	if (ret < GP_OK) {
+		fprintf (stderr, "could not set widget value: %d\n", ret);
+		goto out;
+	}
+	ret = gp_camera_set_single_config (camera, key, child, context);
+	if (ret != GP_OK) {
+		/* This stores it on the camera again */
+		ret = gp_camera_set_config (camera, widget, context);
+		if (ret < GP_OK) {
+			fprintf (stderr, "camera_set_config failed: %d\n", ret);
+			return ret;
+		}
+	}
+out:
+	gp_widget_free (widget);
+	return ret;
+}
